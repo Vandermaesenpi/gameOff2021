854dc6c5c6e3461b74181103359207329b19b738
Vandermaesen Pierre <vandermaesenpi@gmail.com>
2021-11-01 22:06:10 +0100

    {Add} GM + player controller skeletons

---
+++ Assets/Scripts/Core/GM.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class GM : SingletonGeneric<GM>
+{
+    public ArtManager art;
+}
---
+++ Assets/Scripts/Core/SingletonGeneric.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SingletonGeneric<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T I {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<T> ();
+                if (instance == null) {
+                    GameObject obj = new GameObject ();
+                    obj.name = typeof(T).Name;
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+}
